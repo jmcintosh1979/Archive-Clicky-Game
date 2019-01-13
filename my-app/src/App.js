7863f89b03ef929d9f87b8c3878031539615e5c9
MacAttack79 <jmcintosh1979@gmail.com>
2019-01-13 18:16:42 -0500

    added .json file & displayed images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Transformers from './images.json';
+import Wrapper from './components/Wrapper';
+import Title from './components/Title';
+import TransformersCard from './components/TransformersCard';
 
 class App extends Component {
+  // Setting the state for images and score
+  state = {
+    Transformers
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      <Wrapper>
+      <Title>Transformers Clicky Game</Title>
+        {this.state.Transformers.map(images => (
+          <TransformersCard
+            id={images.id}
+            key={images.id}
+            name={images.name}
+            image={images.image}
+          />
+        ))} 
+      </Wrapper>
+    )
+  };
 }
+
 
 export default App;
